0665eab8d81899c677c88e493e6d55ace0f5fd40
LWRGitHub <loganreynoldslwr@gmail.com>
2020-04-15 10:31:25 -0700

    update

--- practice/week-1/javascript/refresh.js
+++ practice/week-1/javascript/refresh.js
@@ -10,7 +10,7 @@ console.log(myFirstVar);
 let myRealFirstVar = true;
 
 const MYFIRSTCONST = "cannotbechange";
-MYFIRSTCONST = "NEWVALUE";
+// NOT POSIBLE MYFIRSTCONST = "NEWVALUE";
 
 //if stament
 if (myFirstVar) {}
@@ -20,7 +20,7 @@ while(false){
 
 }
 
-for(var counter = 0; conter < 10; counter = counter +1){
+for(var counter = 0; counter < 10; counter = counter +1){
 
 }
 
@@ -40,4 +40,85 @@ function myFunctio(myVar, myFirstVar){
 
 }
 
-console.log(myVar);
+//console.log(myVar);
+
+// camparitors, coparison symbols
+// == vs =
+//= is for assignment
+// == copares both sides and create a boolean. If the same value, it becomes false
+
+// 19 == 30
+
+// === compares both sides for value and data type. if either of these are not the same it is false.
+
+100 == "100";
+console.log(10 == "10");
+
+100 === "100";
+console.log(10 === "10");
+
+// < and >, if one value is larger that the other side, then it becomes true.
+
+// <= >+, same as above but allows to be equal.
+// <== >==, same as above but must be the same datatype. (not available in JS)
+// != Should NOT be the same on both sides TO BE TRUE.
+// !== same as above but must be the same datatype.
+
+"t" < 10;
+20 < 10;
+74 < 10;
+
+console.log("hello" !== "hello");
+console.log("t" != 10);
+console.log("t" != 74);
+console.log("t" !== 10);
+74 !== 10;
+
+// Mathematical operators
+// +
+// -
+// *
+// /
+// %
+
+// 1 + 1, 1 - 1, 1*1, 1/1, 1%1;
+
+// --
+// ++
+
+100--; // 99
+100 - 1;
+
+100++; // 101
+100 + 1
+
+
+// +=
+// -=
+// *=
+// /=
+
+var somethig = 10;
+
+somethig += 10;
+something = something + 10;
+
+//Math.ceil(Math.random(Math.PI));
+
+// Logical Operators.
+// || && !
+
+// ||
+true || false;
+
+// &&
+true && true;
+true && false;
+!true // false
+!false // true
+!!false;
+
+true && false && true; // false
+true || false || true; // true
+
+while(true || false) {}
