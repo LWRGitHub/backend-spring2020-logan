53ae75bf5787abdfa82328bc1eaf86481fcd0b1b
LWRGitHub <loganreynoldslwr@gmail.com>
2020-04-16 12:30:02 -0700

    negative numbers fix

--- practice/week-1/javascript/dummyproofsetTimeout.js
+++ practice/week-1/javascript/dummyproofsetTimeout.js
@@ -1,18 +1,38 @@
 // first argument is what to run, second value is how long to wait for in milliseconds.
 
-function dpSetTimeout(instruc, timeoutLength){
-    
-    if(typeof instruc === "function" && typeof timeoutLength === "number"){
-        setTimeout(instruc, timeoutLength)
+function dpSetTimeout(instructions, timeoutLength, count = 1, pause = 0) {
+
+    if(instructions < 0){
+        instructions = 0;
+    }
+    if(timeoutLength < 0){
+        timeoutLength = 0;
+    }
+    if (count < 0){
+        count = 0;
     }
 
+    if (typeof instructions === "function" && typeof timeoutLength === "number") {
+
+        for (let i = 0; i < count; i++) {
+
+            let pauseLength = pause * i;
+
+            setTimeout(instructions, timeoutLength + pauseLength);
+
+        }
+
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
-function mySpecialFunc(){
+function mySpecialFunction() {
     console.log("running something here");
 }
 
-dpSetTimeout(mySpecialFunc, 2000);
+dpSetTimeout(mySpecialFunction, -2000, -6, -1000);
 
 
-console.log("The script has finished!")
+console.log("The script has finished!");
