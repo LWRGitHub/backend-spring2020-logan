ca9558db827deaf6ee013a0b33e14697d1ff1c5e
LWRGitHub <loganreynoldslwr@gmail.com>
2020-04-22 11:45:25 -0700

    JSON learning & testing writing to file

--- practice/week-2/node/persistentObject.js
+++ practice/week-2/node/persistentObject.js
@@ -1,12 +1,14 @@
 // Topics used in this file are: FS Package, JSON, JavaScript objects.
 
 const fs = require("fs");
+
 let persistentObject = null;
 
 let filename = "persistentObject.json";
 
 let doesFileExist = fs.existsSync(filename);
 
+// If exists...
 if(doesFileExist){
     let existingJSON = fs.readFileSync(filename);
     console.log(`Read file: ${existingJSON}`);
@@ -17,13 +19,28 @@ if(doesFileExist){
     console.log("File does not exist, creating new file");
     fs.writeFileSync(filename, `{"numberslist": []}`, "utf8");
     persistentObject = {numbersList: []};
+} 
+
+// The following makes sure that we have something in the arguments to store.
+
+let valueToAdd = process.argv[2];
+
+if(valueToAdd === undefined){
+    console.log("You need to provide a number after the filename to add to the list of numbers!");
+    return;
+} else if(isNaN(valueToAdd)){
+    console.log("The value you provided is not a number!");
+} else {
+
+    valueToAdd = parseInt(valueToAdd);
+    //Add the number provided in the terminal & push it into the array held by the Object.
+    persistentObject.numbersList.push( valueToAdd );
+
+    // Convert the persistentObject to a JSON string
+    let data = JSON.stringify(persistentObject);
+
+    //REPLACE the JSON file with 
+    fs.writeFileSync(filename, data, "utf8");
+
+    console.log(persistentObject);
 }
-
-persistentObject.numbersList.push( process.argv[2] );
-
-let data = JSON.stringify(persistentObject);
-
-fs.writeFileSync(filename, data, "utf8");
-
-console.log(persistentObject);
-
