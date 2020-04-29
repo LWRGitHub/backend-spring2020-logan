9c14443e7ebdfc7c4a32a2dcc9a91c81392670a8
LWRGitHub <loganreynoldslwr@gmail.com>
2020-04-29 14:46:02 -0700

    update

--- practice/week-3/express/firstServer/index.js
+++ practice/week-3/express/firstServer/index.js
@@ -1,5 +1,5 @@
 // Including the Express package for our script.
-const express = require("espress");
+const express = require("express");
 
 // Run the Express server.
 const app = express();
@@ -10,8 +10,15 @@ const http = require("http").Server(app);
 // The port we are going to listen on.
 const port = 8080;
 
+// Tells HTTP what port to listen to.
+http.listen(port);
+
 // Express Routes
 // epress.static("./public_html") -> used to tell Epress that it is a directory/folder.
 
-app.use( "/client", epress.static( "./public_html" ) );
+app.use( "/client", express.static( "./public_html") );
+app.use( "/secretwebpage", express.static("./secret") );
+app.use( "/", express.static("./publec_html") );
 
+// Signify Express Server is running.
+console.log(`Express is now running on port ${port}`);
