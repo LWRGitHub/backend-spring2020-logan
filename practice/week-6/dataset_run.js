869b03923ed573446e02959b3ace605c7a5aeed7
LWRGitHub <loganreynoldslwr@gmail.com>
2020-05-22 10:14:52 -0700

    update

--- practice/week-6/dataset_run.js
+++ practice/week-6/dataset_run.js
@@ -56,7 +56,7 @@ for(let i = 0; i < statisticArray.length; i++){
 }
 
 // Question 1 START
-console.log("What race has the majority of the mortal injuries.");
+console.log("Q) What race has the majority of the mortal injuries?");
 
 let allInjuriesTotal = [];
 
@@ -92,6 +92,72 @@ for(let i = 0; i < allInjuriesTotal.length; i++){
     }
 }
 
-console.log("The highest deaths based on the data is " + highestDeathObject.race + " with a total of " + highestDeathObject.totalDeaths + " deaths.");
+console.log("A) The highest deaths based on the data is " + highestDeathObject.race + " with a total of " + highestDeathObject.totalDeaths + " deaths.");
 
 //Question 1 END
+
+//Qestion 2
+// console.log("Q) What percetage of injuries where confirmed accidents?");
+
+// let accidents = 0;
+// let nonAccidents = 0;
+// let undetermined = 0;
+
+// for(let i = 0; i < filteredStatistecArray.length; i++){
+
+//     let currentStat = filteredStatistecArray[i];
+
+//     if(currentStat.intention === "Unintentional"){
+//         accidents += currentStat.deaths;
+//     } else if(currentStat.intention === "Undetermined"){
+//         undetermined = undetermined + currentStat.deaths;
+//     } else {
+//         nonAccidents = nonAccidents + currentStat.deaths;
+//     }
+
+
+// }
+
+// let accidentPercentage = (accidents / (nonAccidents + accidents)) * 100;
+// accidentPercentage = number.toFixed(2);
+
+// console.log("A) The percentage of accidents is " + accidentPercentage + "%, ignoring undetermined deaths. (Leagal intervention/war considered intentional)");
+
+//Q 2 END
+
+//Q3 
+
+console.log("Q) For makes, which age group has the highest suicide rate?");
+
+let ageGroupDeathTotals = [];
+
+for(let i = 0; i < filteredStatistecArray.length; i++){
+
+    let currentStat = filteredStatistecArray[i];
+
+    if(currentStat.sex === "female"){
+        continue;
+    }
+
+    let createObject = true;
+    
+    for(let e = 0; e < ageGroupDeathTotals.length; e++){
+        if(currentStat.ageGroup === ageGroupDeathTotals.ageGroup){
+            ageGroupDeathTotals[e].deathTotals = ageGroupDeathTotals[e].deathTotals + currentStat.deaths;
+
+            createObject = false;
+            break;
+        }
+    }
+
+    if(createObject){
+        ageGroupDeathTotals.push({
+            ageGroup: currentStat.ageGroup,
+            deathTotals: currentStat.deaths
+        });
+    }
+}
+
+console.log()
+
+//Q3 END
